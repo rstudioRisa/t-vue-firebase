170d9469e89a31fb45114c6ef309c2f189e0de7b
YuguchiRisa <ryuguchi@healthcare.ne.jp>
2018-08-08 20:35:55 +0900

    プラグインを追加

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="app">
+    <Home v-if="!isLogin"></Home>
+    <Editor v-if="isLogin"></Editor>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import Home from "components/Home.vue";
+    import Editor from "components/Editor.vue";
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+
+    export default {
+        name: 'app',
+        data() {
+            return {
+                isLogin: false,
+            }
+
+        },
+        components: {
+            Home,
+            Editor,
+        },
+    }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <button>Googleアカウントでログイン</button>
+  </div>
+  
+</template>
+
+<script>
+    export default {
+        name: "Home",
+        data() {
+            return {
+                msg: "Welcome to Vue-Firebose Test"
+            }
+        }
+    }
+</script>
